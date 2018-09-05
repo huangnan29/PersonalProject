0511f7c21a4ae1204ccd84ac932b95f5fd286184
huangnan <hn@runisys.com>
2018-09-05 19:37:12 +0800

    修改zookeeper代码

---
+++ src/main/java/cn/hn/zookeeper/IRegisterCenter4Invoker.java
@@ -0,0 +1,29 @@
+package cn.hn.zookeeper;
+
+import java.util.Map;
+
+/**
+ * @author: hn
+ * @email : hn@runisys.com
+ * @date : 18-9-5 下午12:00
+ **/
+public interface IRegisterCenter4Invoker {
+    /**
+     *
+     */
+    public void initProviderMap();
+
+
+    /**
+     *
+     */
+    public Map<String List<ProviderService>> getServiceMetaDataMap4Consume();
+
+
+    /**
+     *
+     */
+
+    public void registerInvoker(final InvokerService invoker);
+
+}
---
+++ src/main/java/cn/hn/zookeeper/IRegisterCenter4Provider.java
@@ -0,0 +1,22 @@
+package cn.hn.zookeeper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: hn
+ * @email : hn@runisys.com
+ * @date : 18-9-5 上午11:55
+ **/
+public interface IRegisterCenter4Provider {
+
+    /**
+     *
+     */
+    public void registerProvider (final List<ProviderService> serviceMetadata);
+
+    /**
+     *
+     */
+    public Map<String,List<ProviderService>> getProviderServiceMap();
+}
